0f49041d083f410cc742fb4e5501f6c59e6516d6
donmowgli <akseli.jaara@gmail.com>
2022-05-16 15:28:07 +0300

    tehtävä 3.2. valmiina

--- phonebook-serv/index.js
+++ phonebook-serv/index.js
@@ -1,25 +1,27 @@
 const express = require('express');
 
 
-let notes = [
-    {    
-        id: 1,    
-        content: "HTML is easy",    
-        date: "2022-01-10T17:30:31.098Z",    
-        important: true  
-    },  
-    {    
-        id: 2,    
-        content: "Browser can execute only Javascript",    
-        date: "2022-01-10T18:39:34.091Z",    
-        important: false  
-    },  
-    {    
-        id: 3,    
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        date: "2022-01-10T19:20:14.298Z",    
-        important: true  
-    }
+let persons = [
+    { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+      }
 ];
 
 const app = express();
@@ -30,6 +32,11 @@ app.get('/', (req, res) => {
 
 app.get('/bye', (req, res) => {
     res.send('<h3>Hyvästi muuailma!');
+});
+
+app.get('/info', (req, res) => {
+    let num = persons.length
+    res.send(`<h3>Phonebook has info for ${length} people</h3>`);
 });
 
 app.get('/api/notes', (req, res) => {
