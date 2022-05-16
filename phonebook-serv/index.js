36e8df2ac54c892bfae152af1079305957d7d2c8
anmasnec <annika.sneck@gmail.com>
2022-05-16 16:00:21 +0300

    added 3.3

--- phonebook-serv/index.js
+++ phonebook-serv/index.js
@@ -2,20 +2,18 @@ const express = require('express');
 
 
 let persons = [
-<<<<<<< HEAD
-=======
     {    
         id: 1,    
         name: "Arto Hellas",
         number: "040-123456"
     },  
     {    
-        id: 1,    
+        id: 2,    
         name: "Ada Lovelace",
         number: "39-44-5523523"
     },  
     {    
-        id: 1,    
+        id: 3,    
         name: "Dan Abramov",
         number: "12-43-234345"
     },
@@ -24,7 +22,6 @@ let persons = [
         name: "Mary Poppendick",
         number: "39-23-234345"
     }
->>>>>>> bee6a44f9ef9bf0c1a9cd13b33f9fdff92a52ddf
 ];
 
 const app = express();
@@ -37,19 +34,17 @@ app.get('/bye', (req, res) => {
     res.send('<h3>Hyvästi muuailma!');
 });
 
-<<<<<<< HEAD
-app.get('/info', (req, res) => {
-    let num = persons.length
-    res.send(`<h3>Phonebook has info for ${length} people</h3>`);
-});
-
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-=======
 app.get('/api/persons', (req, res) => {
     res.json(persons);
->>>>>>> bee6a44f9ef9bf0c1a9cd13b33f9fdff92a52ddf
 });
+
+app.get('api/persons/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const person = persons.find(person => person.id === id)
+    console.log(person)
+    res.json(person)
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
