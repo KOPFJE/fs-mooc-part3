7002237fe9e88cc7886e44f41ec75d27ff791ff3
donmowgli <akseli.jaara@gmail.com>
2022-05-16 15:31:46 +0300

    3.2. valmis

--- phonebook-serv/index.js
+++ phonebook-serv/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 
 let persons = [
-<<<<<<< HEAD
-=======
     {    
         id: 1,    
         name: "Arto Hellas",
@@ -24,7 +22,6 @@ let persons = [
         name: "Mary Poppendick",
         number: "39-23-234345"
     }
->>>>>>> bee6a44f9ef9bf0c1a9cd13b33f9fdff92a52ddf
 ];
 
 const app = express();
@@ -37,7 +34,6 @@ app.get('/bye', (req, res) => {
     res.send('<h3>Hyvästi muuailma!');
 });
 
-<<<<<<< HEAD
 app.get('/info', (req, res) => {
     let num = persons.length
     res.send(`<h3>Phonebook has info for ${length} people</h3>`);
@@ -45,10 +41,6 @@ app.get('/info', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-=======
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
->>>>>>> bee6a44f9ef9bf0c1a9cd13b33f9fdff92a52ddf
 });
 
 const PORT = 3001;
