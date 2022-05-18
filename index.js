45255db36750a5cc79b7a491f96c169942dda3f7
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-05-18 14:16:15 +0300

    uibuild

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ let persons = [
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('body', (req, res) => { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
