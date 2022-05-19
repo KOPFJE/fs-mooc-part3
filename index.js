bf629ed91a62c8018124241bba5325c8b98b4696
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-05-19 13:10:49 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const Person = require('./models/person');
 const { response } = require('express');
--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 const url = process.env.MONGODB_URI
 mongoose.connect(url)
     .then(result => {
